72883bc9d5083ebd0bb0373c812f94298111e913
Anders F Björklund <anders.f.bjorklund@gmail.com>
2021-05-23 11:47:00 +0200

    Add support for username and password and api

--- dump-couchbase.py
+++ dump-couchbase.py
@@ -5,12 +5,15 @@ import sys
 import os
 
 config = os.getenv("COUCHBASE_CONF", 'couchbase://localhost/default')
+username = os.getenv("COUCHBASE_USERNAME", "")
+password = os.getenv("COUCHBASE_PASSWORD", "")
 from couchbase.bucket import Bucket
-bucket = Bucket(config)
+bucket = Bucket(config, username=username, password=password)
 
 key = sys.argv[1]
 val = bucket.get(key, quiet=True, no_format=True)
 if val.value:
-    print val.value
+    print "%s: %d" % (key, len(val.value))
+
 else:
     print "key missing"
--- upload-couchbase.py
+++ upload-couchbase.py
@@ -4,8 +4,10 @@ import couchbase
 import os
 
 config = os.getenv("COUCHBASE_CONF", 'couchbase://localhost/default')
+username = os.getenv("COUCHBASE_USERNAME", "")
+password = os.getenv("COUCHBASE_PASSWORD", "")
 from couchbase.bucket import Bucket
-bucket = Bucket(config)
+bucket = Bucket(config, username=username, password=password)
 
 ccache = os.getenv("CCACHE_DIR", os.path.expanduser("~/.ccache"))
 filelist = []
@@ -15,7 +17,7 @@ for dirpath, dirnames, filenames in os.walk(ccache):
         stat = os.stat(os.path.join(dirpath, filename))
         filelist.append((stat.st_mtime, dirpath, filename))
 filelist.sort()
-files = blobs = toobig = objects = manifest = 0
+files = docs = toobig = objects = manifest = 0
 for mtime, dirpath, filename in filelist:
     dirname = dirpath.replace(ccache + os.path.sep, "")
     if filename == "CACHEDIR.TAG":
@@ -37,10 +39,11 @@ for mtime, dirpath, filename in filelist:
             if val:
                 print "%s: %d" % (key, len(val))
                 try:
-                    bucket.upsert(key, val)
-                    blobs = blobs + 1
-                except couchbase.exceptions.TooBigError:
+                    from couchbase_core._libcouchbase import FMT_BYTES # format missing ?
+                    bucket.upsert(key, val, format=FMT_BYTES)
+                    docs = docs + 1
+                except couchbase.exceptions.TooBigException:
                     print "# TOO BIG! (%dM)" % (len(val) >> 20)
                     toobig = toobig + 1
             files = files + 1
-print "%d files, %d objects (%d manifest) = %d blobs (%d toobig)" % (files, objects, manifest, blobs, toobig)
+print "%d files, %d objects (%d manifest) = %d docs (%d toobig)" % (files, objects, manifest, docs, toobig)
